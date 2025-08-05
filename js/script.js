c7ad21572412af5c052b5eed6ce1b212ecd13572
Dudu Valera <duduvalera14@gmail.com>
2025-08-04 23:44:16 -0300

    pequena refatoração no fetchPage para funcionamento no opção de recuperação de senha na página reset

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import initAnimaLogo from "./modules/animeHeader.js";
 import initContatoForm from "./modules/contato.js";
 import initAccordion from "./modules/accordion.js";
 import initLogin from "./modules/login.js";
+import initResetPassword from "./modules/reset.js";
 initFetchPage();
 initAnimaLogo();
 initContatoForm();
@@ -10,4 +11,7 @@ initAccordion();
 
 if (window.location.pathname.includes("login.html")) {
   initLogin();
+}
+if (window.location.pathname.includes("reset.html")) {
+  initResetPassword();
 }
