5ffc40d0ec834296eb66637687389d207f504e25
Dudu Valera <duduvalera14@gmail.com>
2025-08-12 01:36:06 -0300

    adição de import dinâmico para os arquivos js de contato, login, reset e perfil

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 import initAnimaLogo from "./modules/animeHeader.js";
 import initMenu from "./modules/menu.js";
-import initContatoForm from "./modules/contato.js";
 import initAccordion from "./modules/accordion.js";
-import initLogin from "./modules/login.js";
-import initResetPassword from "./modules/reset.js";
-import initPerfil from "./modules/perfil.js";
 initMenu();
 initAnimaLogo();
-initContatoForm();
 initAccordion();
 
+if (window.location.pathname.includes("contato.html")) {
+  const { default: initContatoForm } = await import("./modules/contato.js");
+  initContatoForm();
+}
+
 if (window.location.pathname.includes("login.html")) {
+  const { default: initLogin } = await import("./modules/login.js");
   initLogin();
 }
 
 if (window.location.pathname.includes("reset.html")) {
+  const { default: initResetPassword } = await import("./modules/reset.js");
   initResetPassword();
 }
 
 if (window.location.pathname.includes("perfil.html")) {
+  const { default: initPerfil } = await import("./modules/perfil.js");
   initPerfil();
 }
